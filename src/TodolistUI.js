8fe04e1b0e20c6853c69a628a2926e315f7fba8c
ngm <niuguimin@bonc.com.cn>
2019-03-17 22:45:34 +0800

    ui改为无状态组件

--- src/TodolistUI.js
+++ src/TodolistUI.js
@@ -2,20 +2,17 @@ import React , { Component, Fragment } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component {
-
-  
-  render(){
-    return (
+const TodoListUI = (props)=> {
+  return (
       <Fragment>
         <div style={{margin:'20px'}}>
           <label htmlFor="insertArea" className="input-label">输入内容</label>
           <Input placeholder="todo info" style={{width:'300px'}}
-          id="insertArea" value={this.props.inputValue} 
-          onChange={this.props.handleInputChange}
-          ref={(input)=> this.aainput = input}
+          id="insertArea" value={props.inputValue} 
+          onChange={props.handleInputChange}
+          //ref={(input)=> this.aainput = input}
           ></Input>
-          <Button type="primary" onClick={this.props.handleBtnClick}>提交</Button>
+          <Button type="primary" onClick={props.handleBtnClick}>提交</Button>
           
  
         </div>
@@ -23,9 +20,9 @@ class TodoListUI extends Component {
           <List
             style={{width:'300px', margin:'20px'}}
             bordered
-            dataSource={this.props.inputList}
+            dataSource={props.inputList}
             renderItem={ (item, index) => (<List.Item onClick={ ()=> {
-              this.props.handleDelete(index) 
+              props.handleDelete(index) 
             } }>{item}</List.Item>) }
             
           ></List>
@@ -34,9 +31,7 @@ class TodoListUI extends Component {
           {/* 不转译的显示数据 dangerouslySetInnerHTML={{ __html:item }} */}
 
       </Fragment>
-    )
-  }
-
+  )
 }
 
 export default TodoListUI;
